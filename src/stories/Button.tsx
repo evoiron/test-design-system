2580ef2de4701e2986e899079c8beb61eaa166bf
emile <e.voiron-levy@cacom.fr>
2023-09-26 09:21:36 +0200

    testPR

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -43,7 +43,7 @@ export const Button = ({
       className={["storybook-button", `storybook-button--${size}`, mode].join(
         " "
       )}
-      style={{ backgroundColor, padding: "50px 20px" }}
+      style={{ backgroundColor }}
       {...props}
     >
       {label}
