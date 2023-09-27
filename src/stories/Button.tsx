c1ecbcd8a764d3a316a399a004229699a9913b95
emile <e.voiron-levy@cacom.fr>
2023-09-27 11:52:24 +0200

    testCI

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -43,7 +43,7 @@ export const Button = ({
       className={["storybook-button", `storybook-button--${size}`, mode].join(
         " "
       )}
-      style={{ backgroundColor, backgroundColor: "green" }}
+      style={{ backgroundColor }}
       {...props}
     >
       {label}
